e7ad938addec9be96adbd08710da457c2a4601b3
4Source <38220764+4Source@users.noreply.github.com>
2025-08-16 21:08:37 +0200

    Initial renaming an cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { Plugin } from 'obsidian';
-import { DEFAULT_SETTINGS, Settings } from './settings/SettingsInterface';
-import { MyPluginSettingTab } from './settings/SettingsTab';
 
-export default class MyPlugin extends Plugin {
-	settings: Settings;
+export default class FavoritesPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new MyPluginSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -17,10 +11,10 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+
 	}
 }
