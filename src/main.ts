1113ceab27060b6d3c431b4ae932f927385626a9
4Source <38220764+4Source@users.noreply.github.com>
2025-10-31 17:41:21 +0100

    Fix uninstall

--- src/main.ts
+++ src/main.ts
@@ -9,203 +9,235 @@ export default class FavoritesPlugin extends Plugin {
 	favoriteThemes?: string[];
 	communityPluginModalPrototype?: CommunityModal;
 	communityThemesModalPrototype?: CommunityModal;
-	uninstallModalOpen: () => void;
-	uninstallPluginBrowserModalUpdateItems: () => void;
-	uninstallPluginBrowserModalShowItem: () => void;
-	uninstallThemeBrowserModalUpdateItems: () => void;
-	uninstallThemeBrowserModalShowItem: () => void;
+	uninstallModalOpen?: () => void;
+	uninstallPluginBrowserModalUpdateItems?: () => void;
+	uninstallPluginBrowserModalShowItem?: () => void;
+	uninstallThemeBrowserModalUpdateItems?: () => void;
+	uninstallThemeBrowserModalShowItem?: () => void;
 
 	async onload() {
 		await this.loadSettings();
 
-		this.pluginsKey = process.env.FAVORITE_PLUGINS_KEY || "";
-		this.themesKey = process.env.FAVORITE_THEMES_KEY || "";
-		console.log(this.pluginsKey, this.themesKey)
+		this.pluginsKey = process.env.FAVORITE_PLUGINS_KEY || '';
+		this.themesKey = process.env.FAVORITE_THEMES_KEY || '';
+		console.debug(`Plugins key: ${this.pluginsKey} Themes key: ${this.themesKey}`);
 
 		// eslint-disable-next-line @typescript-eslint/no-this-alias
 		const plugin = this;
 
-		console.debug('Patch Modal.open')
-		this.uninstallModalOpen = around(Modal.prototype, {
-			open(oldMethod) {
-				return dedupe(MONKEY_KEY_MODAL_OPEN, oldMethod, function (...args) {
-					console.debug('Call Modal.open')
-					const result = oldMethod && oldMethod.apply(this, args);
+		if (!this.uninstallModalOpen) {
+			console.debug('Patch Modal.open');
+			this.uninstallModalOpen = around(Modal.prototype, {
+				open(oldMethod) {
+					return dedupe(MONKEY_KEY_MODAL_OPEN, oldMethod, function (...args) {
+						console.debug('Call Modal.open');
+						const result = oldMethod && oldMethod.apply(this, args);
 
-					// Check the opened Modal is the Community plugin browse modal
-					if (this.containerEl?.querySelector('.mod-community-plugin')) {
-						// Patch updateItems method
-						console.debug('Patch PluginBrowserModal.updateItems')
-						plugin.uninstallPluginBrowserModalUpdateItems = around(this.constructor.prototype, {
-							updateItems(oldMethod) {
-								return dedupe(MONKEY_KEY_PLUGIN_BROWSER_MODAL_UPDATE_ITEMS, oldMethod, function () {
-									console.debug('Call PluginBrowserModal.updateItems')
-									// Load the favorite plugins
-									plugin.favoritePlugins = JSON.parse(localStorage.getItem(this.pluginsKey) || '[]');
+						// Check the opened Modal is the Community plugin browse modal
+						if (this.containerEl?.querySelector('.mod-community-plugin')) {
+							// Patch updateItems method
+							if (!plugin.uninstallPluginBrowserModalUpdateItems) {
+								console.debug('Patch PluginBrowserModal.updateItems');
+								plugin.uninstallPluginBrowserModalUpdateItems = around(this.constructor.prototype, {
+									updateItems(oldMethod) {
+										return dedupe(MONKEY_KEY_PLUGIN_BROWSER_MODAL_UPDATE_ITEMS, oldMethod, function () {
+											console.debug('Call PluginBrowserModal.updateItems');
 
-									const result = oldMethod && oldMethod.apply(this);
+											// Load the favorite plugins
+											plugin.favoritePlugins = JSON.parse(localStorage.getItem(plugin.pluginsKey) || '[]');
 
-									// Add to the favorite plugins a tag to visualize it for the user
-									if (plugin.favoritePlugins) {
-										plugin.favoritePlugins.forEach(id => {
-											if (this.items && this.items[id]?.nameEl) {
-												this.items[id].nameEl.createSpan({
-													cls: 'flair',
-													text: 'favorite',
+											const result = oldMethod && oldMethod.apply(this);
+
+											// Add to the favorite plugins a tag to visualize it for the user
+											if (plugin.favoritePlugins) {
+												plugin.favoritePlugins.forEach(id => {
+													if (this.items && this.items[id]?.nameEl) {
+														this.items[id].nameEl.createSpan({
+															cls: 'flair',
+															text: 'favorite',
+														});
+													}
 												});
 											}
+
+											return result;
 										});
-									}
-
-									return result;
+									},
 								});
-							},
-						});
+							}
+							else {
+								console.warn('PluginBrowserModal.updateItems already patched!');
+							}
 
-						// Patch showItem method
-						console.debug('Patch PluginBrowserModal.showItem')
-						plugin.uninstallPluginBrowserModalShowItem = around(this.constructor.prototype, {
-							showItem(oldMethod) {
-								return dedupe(MONKEY_KEY_PLUGIN_BROWSER_MODAL_SHOW_ITEMS, oldMethod, async function (...args: CommunityItem[]) {
-									console.debug('Call PluginBrowserModal.showItem')
-									// Load the favorite plugins
-									plugin.favoritePlugins = JSON.parse(localStorage.getItem(this.pluginsKey) || '[]');
+							// Patch showItem method
+							if (!plugin.uninstallPluginBrowserModalShowItem) {
+								console.debug('Patch PluginBrowserModal.showItem');
+								plugin.uninstallPluginBrowserModalShowItem = around(this.constructor.prototype, {
+									showItem(oldMethod) {
+										return dedupe(MONKEY_KEY_PLUGIN_BROWSER_MODAL_SHOW_ITEMS, oldMethod, async function (...args: CommunityItem[]) {
+											console.debug('Call PluginBrowserModal.showItem');
 
-									return oldMethod && oldMethod.apply(this, args).then(async (r: void) => {
-										const infoEl = await this.detailsEl.getElementsByClassName('community-modal-info')[0];
-										const infoNameEl = await infoEl?.getElementsByClassName('community-modal-info-name')[0];
-										const buttonContainerEl = await infoEl?.getElementsByClassName('community-modal-button-container')[0];
+											// Load the favorite plugins
+											plugin.favoritePlugins = JSON.parse(localStorage.getItem(plugin.pluginsKey) || '[]');
 
-										const isFavorite = plugin.favoritePlugins?.contains(this.selectedItemId);
+											return oldMethod && oldMethod.apply(this, args).then(async (r: void) => {
+												const infoEl = await this.detailsEl.getElementsByClassName('community-modal-info')[0];
+												const infoNameEl = await infoEl?.getElementsByClassName('community-modal-info-name')[0];
+												const buttonContainerEl = await infoEl?.getElementsByClassName('community-modal-button-container')[0];
 
-										// Add to the favorite plugins a tag to visualize it for the user
-										if (isFavorite) {
-											if (infoNameEl) {
-												infoNameEl.createSpan({
-													cls: 'flair',
-													text: 'favorite',
-												});
-											}
-										}
+												const isFavorite = plugin.favoritePlugins?.contains(this.selectedItemId);
 
-										plugin.registerEvent(buttonContainerEl?.createEl('button', { text: isFavorite ? 'Unfavorite' : 'Favorite' }).addEventListener('click', () => {
-											console.log('PluginBrowserModal clicked Favorite')
-											if (isFavorite) {
-												plugin.favoritePlugins?.remove(this.selectedItemId);
-											}
-											else {
-												plugin.favoritePlugins?.push(this.selectedItemId);
-											}
+												// Add to the favorite plugins a tag to visualize it for the user
+												if (isFavorite) {
+													if (infoNameEl) {
+														infoNameEl.createSpan({
+															cls: 'flair',
+															text: 'favorite',
+														});
+													}
+												}
 
-											plugin.saveSettings();
+												plugin.registerEvent(buttonContainerEl?.createEl('button', { text: isFavorite ? 'Unfavorite' : 'Favorite' }).addEventListener('click', () => {
+													if (isFavorite) {
+														plugin.favoritePlugins?.remove(this.selectedItemId);
+													}
+													else {
+														plugin.favoritePlugins?.push(this.selectedItemId);
+													}
 
-											// Redraw
-											infoEl.detach();
-											this.showItem(this.items[this.selectedItemId]);
-											this.update();
-										}));
+													plugin.saveSettings();
 
-										return r;
-									});
-								});
-							},
-						});
-					}
+													// Redraw
+													infoEl.detach();
+													this.showItem(this.items[this.selectedItemId]);
+													this.update();
+												}));
 
-					// Check the opened Modal is the Community themes browse modal
-					if (this.containerEl?.querySelector('.mod-community-theme')) {
-						// Patch updateItems method
-						console.debug('Patch ThemeBrowserModal.updateItems')
-						plugin.uninstallThemeBrowserModalUpdateItems = around(this.constructor.prototype, {
-							updateItems(oldMethod) {
-								return dedupe(MONKEY_KEY_THEME_BROWSER_MODAL_UPDATE_ITEMS, oldMethod, function () {
-									console.debug('Call ThemeBrowserModal.updateItems')
-									// Load the favorite themes
-									plugin.favoriteThemes = JSON.parse(localStorage.getItem(this.themesKey) || '[]');
-
-									const result = oldMethod && oldMethod.apply(this);
-
-									// Ignore the default theme
-									if (this.selectedItemId === '') {
-										return;
-									}
-
-									// Add to the favorite themes a tag to visualize it for the user
-									if (plugin.favoriteThemes) {
-										plugin.favoriteThemes.forEach(id => {
-											if (this.items && this.items[id]?.nameEl) {
-												this.items[id].nameEl.createSpan({
-													cls: 'flair',
-													text: 'favorite',
-												});
-											}
+												return r;
+											});
 										});
-									}
-
-									return result;
+									},
 								});
-							},
-						});
+							}
+							else {
+								console.warn('PluginBrowserModal.showItem already patched!');
+							}
+						}
 
-						// Patch showItems method
-						console.debug('Patch ThemeBrowserModal.showItem')
-						plugin.uninstallThemeBrowserModalShowItem = around(this.constructor.prototype, {
-							showItem(oldMethod) {
-								return dedupe(MONKEY_KEY_THEME_BROWSER_MODAL_SHOW_ITEMS, oldMethod, async function (...args: CommunityItem[]) {
-									console.debug('Call ThemeBrowserModal.showItem')
-									// Load the favorite themes
-									plugin.favoriteThemes = JSON.parse(localStorage.getItem(this.themesKey) || '[]');
+						// Check the opened Modal is the Community themes browse modal
+						if (this.containerEl?.querySelector('.mod-community-theme')) {
+							// Patch updateItems method
+							if (!plugin.uninstallThemeBrowserModalUpdateItems) {
+								console.debug('Patch ThemeBrowserModal.updateItems');
+								plugin.uninstallThemeBrowserModalUpdateItems = around(this.constructor.prototype, {
+									updateItems(oldMethod) {
+										return dedupe(MONKEY_KEY_THEME_BROWSER_MODAL_UPDATE_ITEMS, oldMethod, function () {
+											console.debug('Call ThemeBrowserModal.updateItems');
 
-									return oldMethod && oldMethod.apply(this, args).then(async (r: void) => {
-										// Ignore the default theme
-										if (this.selectedItemId === '') {
-											return;
-										}
+											// Load the favorite themes
+											plugin.favoriteThemes = JSON.parse(localStorage.getItem(plugin.themesKey) || '[]');
 
-										const infoEl = await this.detailsEl.getElementsByClassName('community-modal-info')[0];
-										const infoNameEl = await infoEl?.getElementsByClassName('community-modal-info-name')[0];
-										const buttonContainerEl = await infoEl?.getElementsByClassName('community-modal-button-container')[0];
+											const result = oldMethod && oldMethod.apply(this);
 
-										const isFavorite = plugin.favoriteThemes?.contains(this.selectedItemId);
+											// Ignore the default theme
+											if (this.selectedItemId === '') {
+												return result;
+											}
 
-										// Add to the favorite themes a tag to visualize it for the user
-										if (isFavorite) {
-											if (infoNameEl) {
-												infoNameEl.createSpan({
-													cls: 'flair',
-													text: 'favorite',
+											// Add to the favorite themes a tag to visualize it for the user
+											if (plugin.favoriteThemes) {
+												plugin.favoriteThemes.forEach(id => {
+													if (this.items && this.items[id]?.nameEl) {
+														this.items[id].nameEl.createSpan({
+															cls: 'flair',
+															text: 'favorite',
+														});
+													}
 												});
 											}
-										}
 
-										plugin.registerEvent(buttonContainerEl?.createEl('button', { text: isFavorite ? 'Unfavorite' : 'Favorite' }).addEventListener('click', () => {
-											console.log('ThemeBrowserModal clicked Favorite')
-											if (isFavorite) {
-												plugin.favoriteThemes?.remove(this.selectedItemId);
-											}
-											else {
-												plugin.favoriteThemes?.push(this.selectedItemId);
-											}
-
-											plugin.saveSettings();
-
-											// Redraw
-											infoEl.detach();
-											this.showItem(this.items[this.selectedItemId]);
-											this.update();
-										}));
-
-										return r;
-									});
+											return result;
+										});
+									},
 								});
-							},
-						});
-					}
+							}
+							else {
+								console.warn('ThemeBrowserModal.updateItems already patched!');
+							}
 
-					return result;
-				});
-			},
-		});
+							// Patch showItems method
+							if (!plugin.uninstallThemeBrowserModalShowItem) {
+								console.debug('Patch ThemeBrowserModal.showItem');
+								plugin.uninstallThemeBrowserModalShowItem = around(this.constructor.prototype, {
+									showItem(oldMethod) {
+										return dedupe(MONKEY_KEY_THEME_BROWSER_MODAL_SHOW_ITEMS, oldMethod, async function (...args: CommunityItem[]) {
+											console.debug('Call ThemeBrowserModal.showItem');
+
+											// Load the favorite themes
+											plugin.favoriteThemes = JSON.parse(localStorage.getItem(plugin.themesKey) || '[]');
+
+											return oldMethod && oldMethod.apply(this, args).then(async (r: void) => {
+												// Ignore the default theme
+												if (this.selectedItemId === '') {
+													return r;
+												}
+
+												const infoEl = await this.detailsEl.getElementsByClassName('community-modal-info')[0];
+												const infoNameEl = await infoEl?.getElementsByClassName('community-modal-info-name')[0];
+												const buttonContainerEl = await infoEl?.getElementsByClassName('community-modal-button-container')[0];
+
+												const isFavorite = plugin.favoriteThemes?.contains(this.selectedItemId);
+
+												// Add to the favorite themes a tag to visualize it for the user
+												if (isFavorite) {
+													if (infoNameEl) {
+														infoNameEl.createSpan({
+															cls: 'flair',
+															text: 'favorite',
+														});
+													}
+												}
+
+												plugin.registerEvent(buttonContainerEl?.createEl('button', { text: isFavorite ? 'Unfavorite' : 'Favorite' }).addEventListener('click', () => {
+													if (isFavorite) {
+														plugin.favoriteThemes?.remove(this.selectedItemId);
+													}
+													else {
+														plugin.favoriteThemes?.push(this.selectedItemId);
+													}
+
+													plugin.saveSettings();
+
+													// Redraw
+													infoEl.detach();
+													this.showItem(this.items[this.selectedItemId]);
+													this.update();
+												}));
+
+												return r;
+											});
+										});
+									},
+								});
+							}
+							else {
+								console.warn('ThemeBrowserModal.showItem already patched!');
+							}
+						}
+
+						if (plugin.uninstallModalOpen && plugin.uninstallPluginBrowserModalUpdateItems && plugin.uninstallPluginBrowserModalShowItem && plugin.uninstallThemeBrowserModalUpdateItems && plugin.uninstallThemeBrowserModalShowItem) {
+							console.debug('Uninstall Modal.open');
+							plugin.uninstallModalOpen();
+							plugin.uninstallModalOpen = undefined;
+						}
+						return result;
+					});
+				},
+			});
+		}
+		else {
+			console.warn('Modal.open already patched!');
+		}
 
 		// TODO: Redraw modals if local storage 'favorite-plugins' or 'favorite-themes' changed
 
@@ -217,26 +249,36 @@ export default class FavoritesPlugin extends Plugin {
 	}
 
 	async onunload() {
-		// Restore the original functions
-		if (this.uninstallModalOpen) {
-			console.debug('Uninstall Modal.open')
-			this.uninstallModalOpen();
+		try {
+			// Restore the original functions
+			if (this.uninstallModalOpen) {
+				console.debug('Uninstall Modal.open');
+				this.uninstallModalOpen();
+				this.uninstallModalOpen = undefined;
+			}
+			if (this.uninstallPluginBrowserModalUpdateItems) {
+				console.debug('Uninstall PluginBrowserModal.updateItem');
+				this.uninstallPluginBrowserModalUpdateItems();
+				this.uninstallPluginBrowserModalUpdateItems = undefined;
+			}
+			if (this.uninstallPluginBrowserModalShowItem) {
+				console.debug('Uninstall PluginBrowserModal.showItem');
+				this.uninstallPluginBrowserModalShowItem();
+				this.uninstallPluginBrowserModalShowItem = undefined;
+			}
+			if (this.uninstallThemeBrowserModalUpdateItems) {
+				console.debug('Uninstall ThemeBrowserModal.updateItem');
+				this.uninstallThemeBrowserModalUpdateItems();
+				this.uninstallThemeBrowserModalUpdateItems = undefined;
+			}
+			if (this.uninstallThemeBrowserModalShowItem) {
+				console.debug('Uninstall ThemeBrowserModal.showItem');
+				this.uninstallThemeBrowserModalShowItem();
+				this.uninstallThemeBrowserModalShowItem = undefined;
+			}
 		}
-		if (this.uninstallPluginBrowserModalUpdateItems) {
-			console.debug('Uninstall PluginBrowserModal.updateItem')
-			this.uninstallPluginBrowserModalUpdateItems();
-		}
-		if (this.uninstallPluginBrowserModalShowItem) {
-			console.debug('Uninstall PluginBrowserModal.showItem')
-			this.uninstallPluginBrowserModalShowItem();
-		}
-		if (this.uninstallThemeBrowserModalUpdateItems) {
-			console.debug('Uninstall ThemeBrowserModal.updateItem')
-			this.uninstallThemeBrowserModalUpdateItems();
-		}
-		if (this.uninstallThemeBrowserModalShowItem) {
-			console.debug('Uninstall ThemeBrowserModal.showItem')
-			this.uninstallThemeBrowserModalShowItem();
+		catch (e) {
+			console.error(e);
 		}
 	}
 
